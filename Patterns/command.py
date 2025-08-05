51ac6053c6455b89924442e6dbee4fe6a26ac3f8
RaulConfessor <confessorraul@gmail.com>
2025-08-04 22:46:46 -0300

    Classes Command

--- Patterns/command.py
+++ Patterns/command.py
@@ -10,3 +10,12 @@ class AlugarLivroCommand(Command):
 
     def executar(self):
         return self.book_service.alugar_livro(self.livro_id, self.locatario)
+    
+class DevolverLivroCommand(Command):
+    def __init__(self, book_service, livro_id, locatario):
+        self.book_service = book_service
+        self.livro_id = livro_id
+        self.locatario = locatario
+
+    def executar(self):
+        return self.book_service.devolver_livro(self.livro_id, self.locatario)
